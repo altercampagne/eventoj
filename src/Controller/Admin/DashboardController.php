3bc5caeeb6147b899d2391713d473fd9e1068f26
Olivier Dolbeau <olivier@bbnt.me>
2024-01-14 15:19:19 +0100

    Admin improvements

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_ADMIN')]
+#[Route('/_admin', name: 'admin')]
+class DashboardController extends AbstractController
+{
+    public function __invoke(string $slug = null): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+}
