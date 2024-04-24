2b21d68482700507d944ac786982e61b106d7bcc
Olivier Dolbeau <olivier@bbnt.me>
2024-04-24 15:35:27 +0200

    Add first column in admin dashboard

--- src/Admin/Controller/DashboardController.php
+++ src/Admin/Controller/DashboardController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Admin\Controller;
 
 use App\Admin\Security\Permission;
+use App\Entity\Event;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,8 +16,25 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[Route('/', name: 'admin')]
 class DashboardController extends AbstractController
 {
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+    ) {
+    }
+
     public function __invoke(?string $slug = null): Response
     {
-        return $this->render('admin/dashboard.html.twig');
+        $qb = $this->em->createQueryBuilder();
+        $qb
+            ->select('e, s, p')
+            ->from(Event::class, 'e')
+            ->leftJoin('e.stages', 's')
+            ->leftJoin('e.picture', 'p')
+            ->where('s.date >= :now')
+            ->setParameter('now', new \DateTimeImmutable())
+        ;
+
+        return $this->render('admin/dashboard.html.twig', [
+            'events' => $qb->getQuery()->getResult(),
+        ]);
     }
 }
