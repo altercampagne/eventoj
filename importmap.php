799fd5ff82da24312d1bc6559aa682f83ca0a48d
Olivier Dolbeau <olivier@bbnt.me>
2024-07-15 13:42:16 +0200

    Update vendors

--- importmap.php
+++ importmap.php
@@ -47,10 +47,10 @@ return [
         'type' => 'css',
     ],
     'trix' => [
-        'version' => '2.1.1',
+        'version' => '2.1.2',
     ],
     'trix/dist/trix.min.css' => [
-        'version' => '2.1.1',
+        'version' => '2.1.2',
         'type' => 'css',
     ],
     '@hotwired/stimulus' => [
