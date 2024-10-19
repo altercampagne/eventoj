a0286fd3a88c5269eaf0c17d820f036ef4b10f3e
Olivier Dolbeau <olivier@bbnt.me>
2024-10-19 11:05:15 +0200

    Improve PahekoHttpClient to ease debugging

--- src/Service/Paheko/Client/PahekoHttpClient.php
+++ src/Service/Paheko/Client/PahekoHttpClient.php
@@ -4,50 +4,74 @@ declare(strict_types=1);
 
 namespace App\Service\Paheko\Client;
 
+use Psr\Log\LoggerInterface;
+use Symfony\Contracts\HttpClient\Exception\HttpExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-final class PahekoHttpClient implements PahekoClientInterface
+final readonly class PahekoHttpClient implements PahekoClientInterface
 {
     public function __construct(
-        private readonly HttpClientInterface $pahekoClient,
+        private HttpClientInterface $pahekoClient,
+        private LoggerInterface $logger,
     ) {
     }
 
     public function createUser(array $data): array
     {
-        return $this->pahekoClient->request('POST', 'user/new', [
+        return $this->request('POST', 'user/new', [
             'body' => $data,
-        ])->toArray();
+        ]);
     }
 
     public function updateUser(string $pahekoId, array $data): array
     {
-        return $this->pahekoClient->request('POST', "user/$pahekoId", [
+        return $this->request('POST', "user/$pahekoId", [
             'body' => $data,
-        ])->toArray();
+        ]);
     }
 
     public function getUserCategories(): array
     {
-        return $this->pahekoClient->request('GET', 'user/categories')->toArray();
+        /* @phpstan-ignore-next-line */
+        return $this->request('GET', 'user/categories');
     }
 
     public function getUsersFromCategory(string $categoryId): array
     {
-        return $this->pahekoClient->request('POST', "user/category/$categoryId.json")->toArray();
+        /* @phpstan-ignore-next-line */
+        return $this->request('POST', "user/category/$categoryId.json");
     }
 
     public function createPayment(array $data): array
     {
-        return $this->pahekoClient->request('POST', 'accounting/transaction', [
+        return $this->request('POST', 'accounting/transaction', [
             'body' => $data,
-        ])->toArray();
+        ]);
     }
 
     public function updatePayment(string $pahekoId, array $data): array
     {
-        return $this->pahekoClient->request('POST', "accounting/transaction/$pahekoId", [
+        return $this->request('POST', "accounting/transaction/$pahekoId", [
             'body' => $data,
-        ])->toArray();
+        ]);
+    }
+
+    /**
+     * @param array<string, mixed> $options
+     *
+     * @return array<mixed>
+     */
+    private function request(string $method, string $uri, array $options = []): array
+    {
+        try {
+            return $this->pahekoClient->request('POST', 'user/new', $options)->toArray();
+        } catch (HttpExceptionInterface $e) {
+            $this->logger->error('Error when calling Pahko API.', [
+                'response_code' => $e->getResponse()->getStatusCode(),
+                'response_content' => $e->getResponse()->getContent(false),
+            ]);
+
+            throw $e;
+        }
     }
 }
