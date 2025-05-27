00d950275382d239616b1e189eea6b0cb74ad261
Olivier Dolbeau <olivier@bbnt.me>
2025-05-27 20:56:09 +0200

    Move Helloasso payment sync in a dedicated service

---
+++ src/Service/Helloasso/PaymentSyncReport.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Helloasso;
+
+final readonly class PaymentSyncReport
+{
+    private const string STATUS_WARNING = 'warning';
+
+    private const string STATUS_NOTHING_DONE = 'nothing_done';
+
+    private const string STATUS_UPDATED = 'updated';
+
+    private function __construct(
+        private readonly string $status,
+        public readonly string $message,
+    ) {
+    }
+
+    public static function warning(string $message): self
+    {
+        return new self(self::STATUS_WARNING, $message);
+    }
+
+    public static function nothingDone(string $message): self
+    {
+        return new self(self::STATUS_NOTHING_DONE, $message);
+    }
+
+    public static function updated(string $message): self
+    {
+        return new self(self::STATUS_UPDATED, $message);
+    }
+
+    public function hasBeenUpdated(): bool
+    {
+        return self::STATUS_UPDATED === $this->status;
+    }
+
+    public function isWarning(): bool
+    {
+        return self::STATUS_WARNING === $this->status;
+    }
+
+    public function nothingHasBeenDone(): bool
+    {
+        return self::STATUS_NOTHING_DONE === $this->status;
+    }
+}
---
+++ src/Service/Helloasso/PaymentSynchronizer.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Helloasso;
+
+use App\Entity\Payment;
+use App\Service\Payment\PaymentHandler;
+use App\Service\Payment\PaymentRefundHandler;
+use Doctrine\ORM\EntityManagerInterface;
+use Helloasso\Enums\PaymentState;
+use Helloasso\HelloassoClient;
+use Helloasso\Models\Statistics\OrderDetail;
+
+final readonly class PaymentSynchronizer
+{
+    public function __construct(
+        private readonly HelloassoClient $helloassoClient,
+        private readonly PaymentHandler $paymentHandler,
+        private readonly PaymentRefundHandler $paymentRefundHandler,
+        private readonly EntityManagerInterface $em,
+    ) {
+    }
+
+    /**
+     * Update the given payment according to Helloasso informations if needed.
+     */
+    public function sync(Payment $payment): PaymentSyncReport
+    {
+        if (null === $id = $payment->getHelloassoCheckoutIntentId()) {
+            return PaymentSyncReport::warning('Given payment does not contains an Helloasso checkout intent ID.');
+        }
+
+        $checkoutIntent = $this->helloassoClient->checkout->retrieve((int) $id);
+        if (null === $order = $checkoutIntent->getOrder()) {
+            return PaymentSyncReport::warning('Aucun paiement trouvé chez Helloasso, paiement en erreur ?');
+        }
+
+        $payment->setHelloassoOrderId((string) $order->getId());
+        $payment->setApprovedAt(\DateTimeImmutable::createFromMutable($order->getDate()));
+
+        $this->em->persist($payment);
+        $this->em->flush();
+
+        if ($this->isOrderRefunded($order)) {
+            if ($payment->isRefunded()) {
+                return PaymentSyncReport::nothingDone('Le paiement est remboursé chez Helloasso et chez nous !');
+            }
+
+            $this->paymentRefundHandler->refund($payment);
+
+            return PaymentSyncReport::updated('Le paiement était remboursé chez Helloasso mais pas chez nous. C\'est maintenant corrigé !');
+        }
+
+        if ($payment->isApproved()) {
+            return PaymentSyncReport::nothingDone('Le paiement est accepté des deux côtés !');
+        }
+
+        $this->paymentHandler->handlePaymentSuccess($payment, $order);
+
+        return PaymentSyncReport::updated('Le paiement n\'était pas approuvé chez nous mais maintenant, c\'est bien le cas !');
+    }
+
+    private function isOrderRefunded(OrderDetail $order): bool
+    {
+        foreach ($order->getPayments() as $payment) {
+            if (PaymentState::Refunded === $payment->getState()) {
+                return true;
+            }
+
+            if (PaymentState::Refunding === $payment->getState()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
