cdf975e3c927ce946e35684bb9b22240f72dd49e
Olivier Dolbeau <olivier@bbnt.me>
2025-06-11 22:02:40 +0200

    Update voters

--- src/Admin/Security/PrepaVoter.php
+++ src/Admin/Security/PrepaVoter.php
@@ -7,6 +7,7 @@ namespace App\Admin\Security;
 use App\Entity\Stage;
 use App\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Vote;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 /**
@@ -27,7 +28,7 @@ class PrepaVoter extends Voter
     }
 
     #[\Override]
-    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token, ?Vote $vote = null): bool
     {
         $user = $token->getUser();
 
--- src/Admin/Security/StatsViewerVoter.php
+++ src/Admin/Security/StatsViewerVoter.php
@@ -6,6 +6,7 @@ namespace App\Admin\Security;
 
 use App\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Vote;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 /**
@@ -26,7 +27,7 @@ class StatsViewerVoter extends Voter
     }
 
     #[\Override]
-    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token, ?Vote $vote = null): bool
     {
         $user = $token->getUser();
 
