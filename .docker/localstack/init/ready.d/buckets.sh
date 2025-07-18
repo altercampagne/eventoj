d8390f419ea1d119b82d7d2812044acb5eeb428e
Olivier Dolbeau <olivier@bbnt.me>
2025-07-18 09:18:04 +0200

    Remove bucket cors config to avoid duplicate header in response

--- .docker/localstack/init/ready.d/buckets.sh
+++ .docker/localstack/init/ready.d/buckets.sh
@@ -1,13 +1,2 @@
 #!/usr/bin/env bash
 awslocal s3 mb s3://local-bucket
-awslocal s3api put-bucket-cors --bucket local-bucket --cors-configuration '{
-  "CORSRules": [
-    {
-      "AllowedHeaders": ["*"],
-      "AllowedMethods": ["GET", "PUT", "POST", "DELETE", "HEAD"],
-      "AllowedOrigins": ["*"],
-      "ExposeHeaders": ["ETag"],
-      "MaxAgeSeconds": 3000
-    }
-  ]
-}'
