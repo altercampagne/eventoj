12eb48f1096dbbc32ffe164d81e08533accb7257
Olivier Dolbeau <olivier@bbnt.me>
2026-02-02 09:19:31 +0100

    fix: activate stimulus to get back autocomplete

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,2 +1,3 @@
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
+import { startStimulusApp } from '@symfony/stimulus-bundle';
+
+const app = startStimulusApp();
